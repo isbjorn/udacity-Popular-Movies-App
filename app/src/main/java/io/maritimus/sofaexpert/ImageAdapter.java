e3c6175d720335905356d631d1cebea48ebda9b1
Oleg Akimov <isbjorn@users.noreply.github.com>
2015-06-28 14:26:08 +0300

    Dummy main grid impl

---
+++ app/src/main/java/io/maritimus/sofaexpert/ImageAdapter.java
@@ -0,0 +1,67 @@
+package io.maritimus.sofaexpert;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+    private final int height;
+    private final int width;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+        height = Math.round(mContext.getResources().getDimension(R.dimen.poster_height));
+        width = Math.round(mContext.getResources().getDimension(R.dimen.poster_width));
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+
+
+
+            imageView.setLayoutParams(new GridView.LayoutParams(width, height));
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            //imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7
+    };
+}
--- app/src/main/java/io/maritimus/sofaexpert/MainActivityFragment.java
+++ app/src/main/java/io/maritimus/sofaexpert/MainActivityFragment.java
@@ -1,10 +1,12 @@
 package io.maritimus.sofaexpert;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 
 /**
@@ -18,6 +20,20 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        GridView gridview = (GridView) view.findViewById(R.id.grid_view);
+
+        if (gridview == null) {
+            Log.d("MYLOG", "gridview is null");
+        } else {
+            Log.d("MYLOG", "gridview is NOT null");
+        }
+
+        gridview.setAdapter(new ImageAdapter(getActivity()));
+
+        return view;
     }
+
 }
+
