db14653258bfccf38eb0f9fb4102cc50cbc20099
Oleg Akimov <isbjorn@users.noreply.github.com>
2015-06-28 20:51:40 +0300

    DetailActivity simple impl

---
+++ app/src/main/java/io/maritimus/sofaexpert/DetailActivity.java
@@ -0,0 +1,52 @@
+package io.maritimus.sofaexpert;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import io.maritimus.sofaexpert.model.Movie;
+
+
+public class DetailActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra(Movie.EXTRA_MOVIE)) {
+            Movie movie = new Movie(intent.getBundleExtra(Movie.EXTRA_MOVIE));
+            ((TextView)findViewById(R.id.movie_title)).setText(movie.title);
+            ((TextView)findViewById(R.id.movie_rating)).setText(movie.getRating());
+            ((TextView)findViewById(R.id.movie_overview)).setText(movie.overview);
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
